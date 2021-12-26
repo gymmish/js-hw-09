d5e0d86ed22e4f0c02fe28073e54f2f381e2f897
Gymmish <samaloanastasia@gmail.com>
2021-12-26 20:11:01 +0200

    .

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+const body = document.querySelector("body");
+const startBtn = document.querySelector("[data-start]");
+const stopBtn = document.querySelector("[data-stop]");
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const timerId = 0;
+
+startBtn.addEventListener("click", () => {
+  startBtn.disabled = true;
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+stopBtn.addEventListener("click", () => {
+  stopBtn.disabled = false;
+  clearInterval(timerId);
+});
